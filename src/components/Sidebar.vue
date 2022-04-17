921e78739bb0ff82a4aadad41c0d5fc9700b201a
Decio Neto <deciom.neto10@gmail.com>
2022-04-17 18:05:12 -0300

    refactor: keed old logic, add more data to json

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,7 +1,7 @@
 <script setup>
 import * as gData from '../assets/json/trimmed-dataset.json'
 
-const listYearRange = () => {
+const listYears = () => {
   const dates = gData.map((d) => d.created_at)
 
   for (let i = 0; i < dates.length; i += 1) {
@@ -17,11 +17,58 @@ const listYearRange = () => {
 
   return years
 }
+
+/* Notes to myself:
+ * The function listYears() above
+ * generates the years based only
+ * on the years present in json.
+ *
+ * The function getYearRange() plus
+ * listYearRange() are used to generate
+ * a range from the latest data to
+ * the earliest data present in json.
+ *
+ * The functions below are not optimized.
+ * If I choose to follow using them, I should
+ * turn them into one and use spread array
+ * to make it more performatic.
+
+let minYear = 0
+let maxYear = 0
+
+const getYearRange = () => {
+  const dates = gData.map((d) => d.created_at)
+
+  for (let i = 0; i < dates.length; i += 1) {
+    dates[i] = dates[i].slice(0, 4)
+
+    if (dates[i] < minYear || minYear === 0) {
+      minYear = dates[i]
+    }
+
+    if (dates[i] > maxYear || maxYear === 0) {
+      maxYear = dates[i]
+    }
+  }
+}
+
+const listYearRange = () => {
+  const range = Math.abs(maxYear - minYear)
+  const min = maxYear - range
+  const years = []
+
+  for (let i = maxYear; i >= min; i -= 1) {
+    years.push(i)
+  }
+
+  return years
+}
+*/
 </script>
 
 <template>
   <ul>
-    <li v-for="year in listYearRange()" :key="year">
+    <li v-for="year in listYears()" :key="year">
       <a href="#">{{ year }}</a>
     </li>
   </ul>
