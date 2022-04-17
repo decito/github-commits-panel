1fb17990a9a63bdada1afb6a83ac9effae72aec0
Decio Neto <deciom.neto10@gmail.com>
2022-04-17 17:39:23 -0300

    feat: grab years from json, change folders

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,31 +1,28 @@
+<script setup>
+import * as gData from '../assets/json/trimmed-dataset.json'
+
+const listYearRange = () => {
+  const dates = gData.map((d) => d.created_at)
+
+  for (let i = 0; i < dates.length; i += 1) {
+    dates[i] = dates[i].slice(0, 4)
+  }
+
+  let years = [...new Set(dates)]
+
+  years = dates.reduce((acc, cur) => {
+    if (acc.indexOf(cur) < 0) acc.push(cur)
+    return acc
+  }, [])
+
+  return years
+}
+</script>
+
 <template>
   <ul>
-    <li>
-      <a href="#">2020</a>
-    </li>
-    <li>
-      <a href="#">2019</a>
-    </li>
-    <li>
-      <a href="#">2018</a>
-    </li>
-    <li>
-      <a href="#">2017</a>
-    </li>
-    <li>
-      <a href="#">2016</a>
-    </li>
-    <li>
-      <a href="#">2015</a>
-    </li>
-    <li>
-      <a href="#">2014</a>
-    </li>
-    <li>
-      <a href="#">2013</a>
-    </li>
-    <li>
-      <a href="#">2012</a>
+    <li v-for="year in listYearRange()" :key="year">
+      <a href="#">{{ year }}</a>
     </li>
   </ul>
 </template>
